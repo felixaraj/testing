b114e089ffd745bba47ec98af1d29c17b9f4e0fc
felixaraj <felixaraj2015@gmail.com>
2020-04-07 23:09:37 +0530

    This is the fourth version of corrections made to this R file.

--- testing.R
+++ testing.R
@@ -7,3 +7,10 @@ print("This is good learning from Coursera MOOC platform")
 installed.packages()
 library()
 
+version
+help(ggplot2)
+??ggplot2
+??ggplots2
+install.packages("ggplot2")
+browseVignettes(package = "ggplot2")
+help("ggplot2")
