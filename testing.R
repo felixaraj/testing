f8cd3c08f987b492891c774019c5679ceaec9f57
felixaraj <felixaraj2015@gmail.com>
2020-04-07 23:02:33 +0530

    This is the third correction to this R file

--- testing.R
+++ testing.R
@@ -3,3 +3,7 @@ print("This file is created within RStudio")
 print("And now this file lives in GitHub repository")
 
 print("This is good learning from Coursera MOOC platform")
+
+installed.packages()
+library()
+
